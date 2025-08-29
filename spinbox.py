4e8838cfe780d0069e5eb4bb7fcd8280b97d2cdb
SAM-RGB12789 <ac3709215@gmail.com>
2025-08-29 07:32:31 -0400

    actualizar genero spinbox.py

--- spinbox.py
+++ spinbox.py
@@ -12,4 +12,17 @@ spin.grid(row=0, column=1, padx=10, pady=10)
 boton=tk.Button(ventana, text="Obtener valor", command=mostrarEdad)
 boton.grid(row=1, column=0, padx=10, pady=10)
 
+
+###GENERO
+def mostrarGenero():
+    tk.messagebox.showinfo("Género", f"El género seleccionado es: {genero.get()}")
+labelGenero=tk.Label(ventana, text="Genero")
+labelGenero.grid(row=2, column=0, padx=5, pady=5, sticky="w")
+#Spinbox de texto para género
+genero=tk.Spinbox(ventana, values=("Masculino", "Femenino", "Otro"))
+genero.grid(row=2, column=1, padx=10, pady=10)
+botonGenero=tk.Button(ventana, text="Obtener género", command=mostrarGenero)
+botonGenero.grid(row=3, column=0, padx=10, pady=10)
+
+
 ventana.mainloop()
