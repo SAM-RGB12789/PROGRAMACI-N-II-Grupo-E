7504b44c7e0de1fd462d816c9c82989e8756e436
SAM-RGB12789 <ac3709215@gmail.com>
2025-09-08 08:42:32 -0400

    ACTUALIZACION DE NOTEBOOKu

--- NOTEBOOK.py
+++ NOTEBOOK.py
@@ -27,6 +27,33 @@ def enmascarar_fecha(texto):
     else:
         edadVar.set("")
     return True
+###--------------------- FUNCION PARA GUARDAR ARCHIVE-------------###
+def guardar_en_archivo():
+    with open("paciente.txt","w",encoding="utf=8") as archivo:
+        for paciente in paciente_data:
+            archivo.write(f"{paciente['Nombre']}|{paciente['Fecha de Nacimiento']}|{paciente['Edad']}|"f"{paciente['Genero']}|{paciente['Grupo sanguíneo']}|"f"{paciente['Tipo de Seguro']}|{paciente['Centro Médico']}\n")
+#--------------------- FUNCION PARA CARGAR ARCHIVO ------------------###
+def cargar_desde_archivo():
+    try:
+        with open("paciente.txt","r",encoding="utf=8") as archivo: ###"r" es para leer#
+            paciente_data.clear() #limpiar lista antes de cargar
+            for linea in archivo:
+                datos=linea.strip().split("|")
+                if len(datos)==7:
+                    paciente={
+                        "Nombre": datos[0],
+                        "Fecha de Nacimiento": datos[1],
+                        "Edad": datos[2],
+                        "Genero": datos[3],
+                        "Grupo sanguíneo": datos[4],
+                        "Tipo de Seguro": datos[5],
+                        "Centro Médico": datos[6]
+                    }
+                    paciente_data.append(paciente)
+        cargar_treeview()
+    except FileNotFoundError:
+        open("paciente.txt","w",encoding="utf=8").close() #crear archivo si no existe
+#
 ###--------------------- PACIENTES ------------------------------###
 #lista de pacientes (inicialmente vacia)
 paciente_data=[]
@@ -37,13 +64,15 @@ def registrarPaciente(): #crear diccionario con los datos ingresados
         "Fecha de Nacimiento": fechaN.get(),
         "Edad": edadVar.get(),
         "Genero": genero.get(),
-        "Grupo sanguíneo": entryGrupoSanguineo.get(),
+        "Grupo sanguíneo": comboGrupoSanguineo.get(),
         "Tipo de Seguro": tipo_seguro.get(),
         "Centro Médico": centro_medico.get()
     }
     #AGREGAR PACIENTE A LA LISTA
     paciente_data.append(paciente)
     #CARGAR TREEVIEW
+    guardar_en_archivo()
+    #RECARGAR TREEVIEW
     cargar_treeview()
 ##funcion cargar treeview
 def cargar_treeview():
@@ -124,7 +153,9 @@ frame_pacientes=ttk.Frame(pestanas)
  
 #agregar pestañas al notebook
 pestanas.add(frame_pacientes, text="Pacientes")
- 
+#########
+
+# ###########
 #Mostrar las pestañas en la ventana
 pestanas.pack(expand=True, fill="both")
  
@@ -166,12 +197,15 @@ genero.set("Masculino") #valor por defecto
 radioMasculino=tk.Radiobutton(frame_pacientes, text="Masculino", variable=genero, value="Masculino")
 radioMasculino.grid(row=3, column=1, sticky="w", padx=5, pady=5)
 radioFemenino=ttk.Radiobutton(frame_pacientes, text="Femenino", variable=genero, value="Femenino")
-radioFemenino.grid(row=4, column=1, sticky="w", padx=5)
+radioFemenino.grid(row=3, column=2, sticky="w", padx=0)
  
 #grupo sanguineo
 labelGrupoSanguineo=tk.Label(frame_pacientes, text="Grupo Sanguíneo: ")
 labelGrupoSanguineo.grid(row=4, column=0, sticky="w", pady=5, padx=5)
 entryGrupoSanguineo=tk.Entry(frame_pacientes)
+comboGrupoSanguineo = ttk.Combobox(frame_pacientes, values=["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"], state="readonly")
+comboGrupoSanguineo.grid(row=4, column=1, sticky="w", padx=5, pady=5)
+comboGrupoSanguineo.set("O+")
  
 ##TIPO DE SEGURO
 labelTipoSeguro=tk.Label(frame_pacientes, text="Tipo de Seguro: ")
@@ -275,5 +309,6 @@ for col in ("Nombre", "Especialidad", "Edad", "Teléfono"):
 
 for i in range(4):
     frame_doctores.grid_columnconfigure(i, weight=1)
-
+cargar_desde_archivo()#cargar datos desde el archivo al iniciar la aplicacion
 ventana_principal.mainloop()
+### TAREA FORMULARIO PACIENTE Y FORMULARIO DOCTOR
